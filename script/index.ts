1cebc18d8f26f4f48d10fbc596a553e012d020ca
ShmPt <cacahouettetbs@gmail.com>
2024-06-07 12:28:25 +0200

    feat: add start button

--- script/index.ts
+++ script/index.ts
@@ -1,15 +1,21 @@
-const attackBtn : any = document.querySelector(".attackButton");
-const specialAttackBtn : any = document.querySelector(".specialAttackButton");
-const healBtn : any = document.querySelector(".healButton");
-const abandonBtn : any = document.querySelector(".abandonButton");
-const displayPlayerHealth : any = document.querySelector(".playerHealth");
-const displayMonsterHealth : any = document.querySelector(".ghoulHealth");
+const gameInterface: any = document.querySelector(".gameInterface");
+
+const startBtn: any = document.querySelector('.startButton');
+const gamePad : any = document.querySelector('.gamePad');
+
+const attackBtn: any = document.querySelector(".attackButton");
+const specialAttackBtn: any = document.querySelector(".specialAttackButton");
+const healBtn: any = document.querySelector(".healButton");
+const abandonBtn: any = document.querySelector(".abandonButton");
+
+const displayPlayerHealth: any = document.querySelector(".playerHealth");
+const displayMonsterHealth: any = document.querySelector(".ghoulHealth");
 
 class Arena {
-    playerHealth : number = 100;
-    monsterHealth : number = 100;
+    playerHealth: number = 100;
+    monsterHealth: number = 100;
 
-    defineDamage(min : number, max : number) {
+    defineDamage(min: number, max: number) {
         return Math.floor((Math.random() * max) + min);
     }
 
@@ -23,14 +29,14 @@ class Arena {
     playerAttack() {
         let playerPotency = this.defineDamage(3, 10);
         this.monsterHealth -= playerPotency;
-        if (this.checkEnemyHealth()) {return;};
+        if (this.checkEnemyHealth()) { return; };
         this.monsterAttack();
     }
 
     playerSpecialAttack() {
         let playerPotency = this.defineDamage(10, 20);
         this.monsterHealth -= playerPotency;
-        if (this.checkEnemyHealth()) {return;};
+        if (this.checkEnemyHealth()) { return; };
         this.monsterAttack();
     }
 
@@ -64,19 +70,28 @@ class Arena {
         displayMonsterHealth.innerText = `Monster's health: ${this.monsterHealth}`;
     }
 
+    startGame() {
+        startBtn.style.display = "none"
+        gamePad.style.display = "inline"
+    }
+
     restartGame() {
         this.playerHealth = 100;
         this.monsterHealth = 100;
         this.updateHealthDisplay();
+        startBtn.style.display = "inline"
+        gamePad.style.display = "none"
         return true;
     }
 }
 
 let game = new Arena;
 
-
 attackBtn.addEventListener("click", () => game.playerAttack());
 specialAttackBtn.addEventListener("click", () => game.playerSpecialAttack());
 healBtn.addEventListener("click", () => game.playerHeal());
 abandonBtn.addEventListener("click", () => game.abandon());
 
+gamePad.style.display = 'none';
+
+startBtn.addEventListener("click", () => game.startGame());
