84263d92801910acec696f0cd8d2a8d8429e856c
ShmPt <cacahouettetbs@gmail.com>
2024-06-09 20:48:17 +0200

    test: begin the addition of the logs feature

--- script/index.ts
+++ script/index.ts
@@ -14,9 +14,12 @@ const healthZone : any = document.querySelector(".healthZone");
 const playerHealthBar : any = healthZone.querySelector('div:first-child');
 const monsterHealthBar : any = healthZone.querySelector('div:last-child');
 
+let logSpace : any = document.querySelector('.logs');
+
 class Arena {
     playerHealth: number = 100;
     monsterHealth: number = 100;
+    logs : Array<string> = [];
 
     defineDamage(min: number, max: number) {
         return Math.floor((Math.random() * max) + min);
@@ -25,13 +28,16 @@ class Arena {
     monsterAttack() {
         let monsterPotency = this.defineDamage(5, 10);
         this.playerHealth -= monsterPotency;
+        this.logs.unshift(`The Monster attacks you, causing ${monsterPotency} damage!`)
         this.checkPlayerHealth();
         this.updateHealthDisplay();
+        this.updateLog();
     }
 
     playerAttack() {
         let playerPotency = this.defineDamage(3, 10);
         this.monsterHealth -= playerPotency;
+        this.logs.unshift(`You attack the monster, causing ${playerPotency} damage!`)
         if (this.checkEnemyHealth()) { return; };
         this.monsterAttack();
     }
@@ -39,12 +45,14 @@ class Arena {
     playerSpecialAttack() {
         let playerPotency = this.defineDamage(10, 20);
         this.monsterHealth -= playerPotency;
+        this.logs.unshift(`You attack the monster with all your might, causing ${playerPotency} damage!`)
         if (this.checkEnemyHealth()) { return; };
         this.monsterAttack();
     }
 
     playerHeal() {
         this.playerHealth += 10;
+        this.logs.unshift(`You heal yourself for 10 health!`)
         this.monsterAttack();
     }
 
@@ -74,6 +82,16 @@ class Arena {
 
         playerHealthBar.style.background = `linear-gradient(to right, rgb(55, 0, 255) ${this.playerHealth}%, black ${this.playerHealth + 0.1}%) `;
         monsterHealthBar.style.background = `linear-gradient(to left, rgb(255, 0, 55) ${this.monsterHealth}%, black ${this.monsterHealth - 0.1}%)`;
+    }
+
+    updateLog() {
+        logSpace.innerHTML = ``;
+        
+        for (let n in this.logs) {
+        let logPart = document.createElement('p');
+        logPart.innerText = this.logs[n]; 
+        logSpace.append(logPart);
+        }
     }
 
     startGame() {
