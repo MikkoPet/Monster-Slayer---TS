fb82d16a12b1e2483e1c6a6dd60e6b9ac784c9a4
ShMPt <cacahouettetbs@gmail.com>
2024-06-10 15:37:10 +0200

    feat: add difficulty levels and adjust attacks to balance games

--- script/index.ts
+++ script/index.ts
@@ -1,11 +1,17 @@
 const gameInterface: any = document.querySelector(".gameInterface");
 
-const startBtn: any = document.querySelector('.startButton');
+const startScreen: any = document.querySelector('.startScreen');
 const gamePad : any = document.querySelector('.gamePad');
+
+const easySelector : any = document.querySelector('.easySelector');
+const mediumSelector : any = document.querySelector('.midSelector');
+const hardSelector : any = document.querySelector('.hardSelector');
+const overlord : any = document.querySelector('.overlord');
 
 const attackBtn: any = document.querySelector(".attackButton");
 const specialAttackBtn: any = document.querySelector(".specialAttackButton");
 const healBtn: any = document.querySelector(".healButton");
+const specialHeal: any = document.querySelector(".specialHealBtn");
 const abandonBtn: any = document.querySelector(".abandonButton");
 
 const displayPlayerHealth: any = document.querySelector(".playerHealth");
@@ -20,13 +26,17 @@ class Arena {
     playerHealth: number = 100;
     monsterHealth: number = 100;
     logs : Array<string> = [];
+    monsterMinPotency : number = 5;
+    monsterMaxPotency : number = 10;
+    killCharges : number = 0;
+    powerCharges : number = 0;
 
     defineDamage(min: number, max: number) {
         return Math.floor((Math.random() * max) + min);
     }
 
     monsterAttack() {
-        let monsterPotency = this.defineDamage(5, 10);
+        let monsterPotency = this.defineDamage(this.monsterMinPotency, this.monsterMaxPotency);
         this.playerHealth -= monsterPotency;
         this.logs.unshift(`The Monster attacks you, causing ${monsterPotency} damage!`)
         this.checkPlayerHealth();
@@ -43,10 +53,26 @@ class Arena {
     }
 
     playerSpecialAttack() {
+        this.powerCharges -= 3;
+        if (this.powerCharges < 3) {
+            specialAttackBtn.style.pointerEvents = "none"
+        }
+
         let playerPotency = this.defineDamage(10, 20);
         this.monsterHealth -= playerPotency;
         this.logs.unshift(`You attack the monster with all your might, causing ${playerPotency} damage!`)
         if (this.checkEnemyHealth()) { return; };
+        this.monsterAttack();
+    }
+
+    playerSpecialHeal() {
+        this.powerCharges -= 3;
+        if (this.powerCharges < 3) {
+            specialAttackBtn.style.pointerEvents = "none"
+        }
+
+        this.playerHealth <= 50 ? this.playerHealth += 50 : this.playerHealth = 100;
+        this.logs.unshift(`Your desperation fuels your healing magic, restoring 50 health!`)
         this.monsterAttack();
     }
 
@@ -64,7 +90,9 @@ class Arena {
     checkEnemyHealth() {
         if (this.monsterHealth <= 0) {
             alert(`You have slain the monster!`);
-            this.restartGame();
+            this.monsterHealth = 0;
+            this.updateHealthDisplay();
+            this.returnToMain();
             return true;
         }
     }
@@ -73,6 +101,15 @@ class Arena {
         if (this.playerHealth <= 0) {
             alert(`The monster has slain you!`);
             this.restartGame();
+        }
+
+        this.checkPower();
+    }
+
+    checkPower() {
+        this.powerCharges += 1;
+        if (this.powerCharges >= 3) {
+            specialAttackBtn.style.pointerEvents = "auto"
         }
     }
 
@@ -104,8 +141,45 @@ class Arena {
         }
     }
 
+    setDifficultyEasy() {
+        this.monsterHealth = 100;
+        this.monsterMinPotency = 3;
+        this.monsterMaxPotency = 5;
+        easySelector.classList.replace("monsterButton" , "deadMonster");
+
+        this.startGame();
+    }
+
+    setDifficultyMid() {
+        this.monsterHealth = 100;
+        this.monsterMinPotency = 5;
+        this.monsterMaxPotency = 10;
+        mediumSelector.classList.replace("monsterButton" , "deadMonster");
+
+        this.startGame();
+    }
+
+    setDifficultyHard() {
+        this.monsterHealth = 100;
+        this.monsterMinPotency = 10;
+        this.monsterMaxPotency = 15;
+        hardSelector.classList.replace("monsterButton", "deadMonster");
+
+        this.startGame();
+    }
+
+    setBoss() {
+        this.monsterMinPotency = 8;
+        this.monsterMaxPotency = 10;
+        this.monsterHealth = 500;
+        displayMonsterHealth.innerText = `Monster's health: ${this.monsterHealth}`;
+
+        this.startGame();
+    }
+
     startGame() {
-        startBtn.style.display = "none"
+        this.updateHealthDisplay();
+        startScreen.style.display = "none"
         gamePad.style.display = "inline"
         logSpace.innerHTML = '';
     }
@@ -114,19 +188,42 @@ class Arena {
         this.playerHealth = 100;
         this.monsterHealth = 100;
         this.updateHealthDisplay();
-        startBtn.style.display = "inline"
+        startScreen.style.display = "inline"
         gamePad.style.display = "none"
+        
+        this.resetStyle();
+
         return true;
+    }
+
+    returnToMain() {
+        this.updateHealthDisplay();        
+        startScreen.style.display = "inline"
+        gamePad.style.display = "none"
+        this.killCharges += 1;
+        if ( this.killCharges === 3 ) {
+            overlord.classList.replace('overlord', 'overlordReady');
+        }
+    }
+
+    resetStyle() {
+        hardSelector.classList.replace("deadMonster", "monsterButton");
+        mediumSelector.classList.replace("deadMonster", "monsterButton");
+        easySelector.classList.replace("deadMonster" , "monsterButton");
     }
 }
 
 let game = new Arena;
 
-attackBtn.addEventListener("click", () => game.playerAttack());
-specialAttackBtn.addEventListener("click", () => game.playerSpecialAttack());
-healBtn.addEventListener("click", () => game.playerHeal());
-abandonBtn.addEventListener("click", () => game.abandon());
+attackBtn.addEventListener('click', () => game.playerAttack());
+specialAttackBtn.addEventListener('click', () => game.playerSpecialAttack());
+healBtn.addEventListener('click', () => game.playerHeal());
+specialHeal.addEventListener('click', () => game.playerSpecialHeal());
+abandonBtn.addEventListener('click', () => game.abandon());
 
 gamePad.style.display = 'none';
 
-startBtn.addEventListener("click", () => game.startGame());
+easySelector.addEventListener('click', () => game.setDifficultyEasy());
+mediumSelector.addEventListener('click', () => game.setDifficultyMid());
+hardSelector.addEventListener('click', () => game.setDifficultyHard());
+overlord.addEventListener('click', () => game.setBoss());
